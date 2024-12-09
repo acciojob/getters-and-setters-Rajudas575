743c7a8b85c5773507f24380fa3b37710ec5083e
Rajudas575 <rajudas575@gmail.com>
2024-12-09 11:20:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,51 @@
-//complete this code
+// Person Class Definition
 class Person {
-	this._name = name;  
-    this._age = age; 
-	get name() {
-	    return this._name;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-	set name(name) {
-	    this._name = name;
-	}
-	
-	get age() {
-	    return this._age;
-	}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-
+  // Setter for age
   set age(age) {
     this._age = age;
   }
+
+  // Getter for age
+  get age() {
+    return this._age;
+  }
 }
 
+// Student Class Definition (extends Person)
 class Student extends Person {
-	constructor(name, age) {
-    super(name, age); 
+  constructor(name, age) {
+    super(name, age);
   }
+
+  // Method to log that the student is studying
   study() {
     console.log(`${this.name} is studying`);
   }
 }
 
+// Teacher Class Definition (extends Person)
 class Teacher extends Person {
-	constructor(name, age) {
-	    super(name, age);
-    }
+  constructor(name, age) {
+    super(name, age);
+  }
 
-	teach() {
-	    console.log(`${this.name} is teaching`);
-	}
+  // Method to log that the teacher is teaching
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
+
+// Example usage
 
 const person = new Person("John", 25);
 console.log(person.name); // Output: John
@@ -50,8 +58,3 @@ student.study(); // Output: Alice is studying
 
 const teacher = new Teacher("Bob", 40);
 teacher.teach(); // Output: Bob is teaching
-
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
